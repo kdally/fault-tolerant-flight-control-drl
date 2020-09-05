d8d475c5eecff73b75ec1938f32edea11d990a8d
Killian Dally <k.dally@student.tudelft.nl>
2020-09-05 11:43:12 +0200

    improved env initialization

--- envs/lin/citation.py
+++ envs/lin/citation.py
@@ -1,6 +1,5 @@
 import gym
 import matlab.engine
-import matplotlib.pyplot as plt
 import numpy as np
 
 
@@ -8,16 +7,14 @@ class Citation(gym.Env):
     """Custom Environment that follows gym interface"""
     metadata = {'render.modes': ['graph']}
 
-    def __init__(self, fig_name: object = 'lin', time_vector: np.ndarray = np.arange(0, 20, 0.01),
-                 evaluation: object = False):
+    def __init__(self, time_vector: np.ndarray = np.arange(0, 20, 0.01), evaluation: bool = False):
+
         super(Citation, self).__init__()
 
-        self.fig_name = fig_name
         self.A_matrix, self.B_matrix = self.get_eom()
 
         # Integration step from EOM using Euler Integration
         self.euler = lambda x, u: x + (self.A_matrix.dot(x) + self.B_matrix.dot(u)) * self.dt
-        # todo: runge-kutta
 
         self.time = time_vector
         self.dt = self.time[1] - self.time[0]
@@ -27,10 +24,15 @@ class Citation(gym.Env):
         self.observation_space = gym.spaces.Box(-3000, 3000, shape=(self.ref_signal.shape[0],), dtype=np.float64)
         self.action_space = gym.spaces.Box(-1., 1., shape=(3,), dtype=np.float64)
 
-    def step(self, action):
+        self.state = None
+        self.state_history = None
+        self.action_history = None
+        self.error = None
+        self.step_count = None
+
+    def step(self, action: np.ndarray):
 
         self.state = self.euler(self.state, self.scale_a(action))
-        self.state = self.boundaries(self.state)
 
         self.error = self.ref_signal[:, self.step_count] - self.state[:self.ref_signal.shape[0]]
         self.state_history[:, self.step_count] = self.state
@@ -50,15 +52,51 @@ class Citation(gym.Env):
         self.step_count = 0
         return np.zeros(self.observation_space.shape)
 
-    def boundaries(self, state):
+    def get_reward(self):
 
-        # state[1] = self.clip(state[1], self.cart.x_min, self.cart.x_max)
-        # state[4] = self.clip(state[4], self.ball.th_min, self.ball.th_max)
+        return -abs(self.clip(self.error.sum()/60, -1, 1))
 
-        return state
+    def get_task(self, evaluation):
 
-    def close(self):
-        pass
+        if evaluation:
+            ref_pbody = np.hstack([np.zeros(int(5 * self.max_steps / 20)),
+                                   2 * np.ones(int(5 * self.max_steps / 20)),
+                                   np.zeros(int(5 * self.max_steps / 20)),
+                                   -2 * np.ones(int(5 * self.max_steps / 20)),
+                                   ])
+            ref_qbody = np.hstack([3 * np.ones(int(4 * self.max_steps / 20)),
+                                   np.zeros(int(4 * self.max_steps / 20)),
+                                   -3 * np.ones(int(4 * self.max_steps / 20)),
+                                   np.zeros(int(4 * self.max_steps / 20)),
+                                   3 * np.ones(int(4 * self.max_steps / 20)),
+                                   ])
+            ref_rbody = np.hstack([1 * np.ones(int(4 * self.max_steps / 20)),
+                                   np.zeros(int(4 * self.max_steps / 20)),
+                                   -1 * np.ones(int(4 * self.max_steps / 20)),
+                                   np.zeros(int(4 * self.max_steps / 20)),
+                                   1 * np.ones(int(4 * self.max_steps / 20)),
+                                   ])
+
+        else:
+            ref_pbody = np.hstack([np.zeros(int(5 * self.max_steps / 20)),
+                                   2 * np.ones(int(5 * self.max_steps / 20)),
+                                   np.zeros(int(5 * self.max_steps / 20)),
+                                   -2 * np.ones(int(5 * self.max_steps / 20)),
+                                   ])
+            ref_qbody = np.hstack([3 * np.ones(int(4 * self.max_steps / 20)),
+                                   np.zeros(int(4 * self.max_steps / 20)),
+                                   -3 * np.ones(int(4 * self.max_steps / 20)),
+                                   np.zeros(int(4 * self.max_steps / 20)),
+                                   3 * np.ones(int(4 * self.max_steps / 20)),
+                                   ])
+            ref_rbody = np.hstack([1 * np.ones(int(4 * self.max_steps / 20)),
+                                   np.zeros(int(4 * self.max_steps / 20)),
+                                   -1 * np.ones(int(4 * self.max_steps / 20)),
+                                   np.zeros(int(4 * self.max_steps / 20)),
+                                   1 * np.ones(int(4 * self.max_steps / 20)),
+                                   ])
+
+        return np.vstack([ref_pbody, ref_qbody, ref_rbody])
 
     def get_obs(self):
 
@@ -66,9 +104,12 @@ class Citation(gym.Env):
         observation = self.error
         return self.scale_o(observation)
 
-    def get_reward(self):
+    def scale_a(self, action):  # z-normalization
 
-        return -abs(self.clip(self.error.sum(), -1, 1))
+        action[0] = self.clip(action[0], -20.05, 14.90)
+        action[1] = self.clip(action[1], -37.24, 37.24)
+        action[2] = self.clip(action[2], -21.77, 21.77)
+        return action
 
     @staticmethod
     def scale_o(obs):  # z-normalization
@@ -79,13 +120,6 @@ class Citation(gym.Env):
         # obs[3] = obs[3] /
 
         return obs
-
-    def scale_a(self, action):  # z-normalization
-
-        action[0] = self.clip(action[0], -20.05, 14.90)
-        action[1] = self.clip(action[1], -37.24, 37.24)
-        action[2] = self.clip(action[2], -21.77, 21.77)
-        return action
 
     @staticmethod
     def clip(value, low, high):
@@ -103,81 +137,11 @@ class Citation(gym.Env):
 
         return A, B
 
-    def get_task(self, evaluation):
+    def render(self, mode='any'):
+        raise NotImplementedError()
 
-        if evaluation:
-            ref_pbody = np.hstack([np.zeros(int(5 * self.max_steps / 20)),
-                                   6 * np.ones(int(5 * self.max_steps / 20)),
-                                   np.zeros(int(5 * self.max_steps / 20)),
-                                   3 * np.ones(int(5 * self.max_steps / 20)),
-                                   ])
-            ref_qbody = np.hstack([3 * np.ones(int(4 * self.max_steps / 20)),
-                                   np.zeros(int(4 * self.max_steps / 20)),
-                                   4 * np.ones(int(4 * self.max_steps / 20)),
-                                   np.zeros(int(4 * self.max_steps / 20)),
-                                   3 * np.ones(int(4 * self.max_steps / 20)),
-                                   ])
-            ref_rbody = np.hstack([3 * np.ones(int(4 * self.max_steps / 20)),
-                                   np.zeros(int(4 * self.max_steps / 20)),
-                                   4 * np.ones(int(4 * self.max_steps / 20)),
-                                   np.zeros(int(4 * self.max_steps / 20)),
-                                   3 * np.ones(int(4 * self.max_steps / 20)),
-                                   ])
-
-        else:
-            ref_pbody = np.hstack([np.zeros(int(5 * self.max_steps / 20)),
-                                   6 * np.ones(int(5 * self.max_steps / 20)),
-                                   np.zeros(int(5 * self.max_steps / 20)),
-                                   3 * np.ones(int(5 * self.max_steps / 20)),
-                                   ])
-            ref_qbody = np.hstack([3 * np.ones(int(4 * self.max_steps / 20)),
-                                   np.zeros(int(4 * self.max_steps / 20)),
-                                   4 * np.ones(int(4 * self.max_steps / 20)),
-                                   np.zeros(int(4 * self.max_steps / 20)),
-                                   3 * np.ones(int(4 * self.max_steps / 20)),
-                                   ])
-            ref_rbody = np.hstack([3 * np.ones(int(4 * self.max_steps / 20)),
-                                   np.zeros(int(4 * self.max_steps / 20)),
-                                   4 * np.ones(int(4 * self.max_steps / 20)),
-                                   np.zeros(int(4 * self.max_steps / 20)),
-                                   3 * np.ones(int(4 * self.max_steps / 20)),
-                                   ])
-
-        return np.vstack([ref_pbody, ref_qbody, ref_rbody])
-
-    def render(self, mode='graph'):
-
-        if mode != 'graph':
-            raise NotImplementedError()
-
-        fig = plt.figure()
-        ax = plt.gca()
-        plt.plot(self.time, self.state_history[1, :].T, label=r'$x_c$ (SAC)', linewidth=2, c='blue')
-        plt.plot(self.time, self.ref_cart, '--', label=r'$x_{ref}$', linewidth=2, c='blue')
-        # plt.plot(self.time, self.PID_history[0, :].T, label=r'$x_c$ (PID)', linewidth=2, c='blue')
-        # plt.plot(self.time, self.state_history[6, :].T, '*-', label=r'$e_x$', linewidth=2, c='blue')
-        plt.plot(self.time, self.state_history[4, :].T, label=r'$\theta$ (SAC)', linewidth=2, c='orange')
-        plt.plot(self.time, self.ref_ball, '--', label=r'$\theta_{ref}$', linewidth=2, c='orange')
-        # plt.plot(self.time, self.PID_history[1, :].T, label=r'$\theta$ (PID)', linewidth=2, c='orange')
-        # plt.plot(self.time, self.state_history[5, :].T, '*-', label=r'$e_\theta$', linewidth=2, c='blue')
-        # plt.plot(self.time, self.state_history[0, :].T, label=r'$x_b$', linewidth=2)
-        # plt.plot(self.time, self.state_history[3, :].T, label=r'$x$', linewidth=2)
-        # plt.plot(time, error_cart, '--', label=r'$error$', linewidth=2, c='red')
-        plt.xlabel('Time [s]', fontsize=11)
-        plt.ylabel('Position [m], [rad]', fontsize=11)
-        plt.legend()
-        ax.set_xlim(0, self.time[-2])
-        ax.set_xticks(np.arange(0, 22, 2))
-        # ax.set_xticks(np.arange(0, 22, 2), minor=True)
-        # ax.set_yticks(np.arange(0, 101, 20))
-        # ax.set_yticks(np.arange(0, 22, 2), minor=True)
-        # ax.grid(which='major', alpha=0.5)
-        ax.grid(which='major')
-        # ax.set_aspect(1.0 / ax.get_data_ratio() * 0.45)
-        plt.savefig(f'models_backup/{self.fig_name}.eps', format='eps')
-        plt.show()
-
-        return
+    def close(self):
+        pass
 
 
 # from stable_baselines.common.env_checker import check_env
--- flying.py
+++ flying.py
@@ -12,23 +12,24 @@ from stable_baselines.sac.policies import LnMlpPolicy
 
 from envs.lin.citation import Citation
 from tools.PID import PID
-from tools.callback import SaveOnBestTrainingRewardCallback
+from stable_baselines.common.callbacks import EvalCallback as SaveOnBestReturn
 from tools.schedule import schedule
 
 warnings.filterwarnings("ignore", category=FutureWarning, module='tensorflow')
 warnings.filterwarnings("ignore", category=UserWarning, module='gym')
 
-env = Citation()
+env_train = Citation(evaluation=False)
+env_eval = Citation(evaluation=True)
 
-learn = False
+learn = True
 
 if learn:
-    env = Monitor(env, "agents/tmp")
-    callback = SaveOnBestTrainingRewardCallback(check_freq=1000, log_dir=log_dir)
-    model = SAC(LnMlpPolicy, env, verbose=1,
+    env_train = Monitor(env_train, "agents/tmp")
+    callback = SaveOnBestReturn(eval_env=env_eval, callback_on_new_best=None, eval_freq=5000, best_model_save_path="agents/tmp/")
+    model = SAC(LnMlpPolicy, env_train, verbose=1,
                 ent_coef='auto', batch_size=256, learning_rate=schedule(0.00094))
 
-    # model = SAC.load("tmp/best_model.zip", envs=envs)
+    # model = SAC.load("tmp/best_model.zip", env=env_train)
     tic = time.time()
     model.learn(total_timesteps=int(5e5), log_interval=10, callback=callback)
     print('')
@@ -37,89 +38,89 @@ if learn:
     del model
 
 # model = SAC.load("agents/500K_simple_batch256.zip")
-# model = SAC.load("agents/tmp/best_model.zip")
+model = SAC.load("agents/tmp/best_model.zip")
 
 # Test the trained agent
-obs = env.reset()
+obs = env_eval.reset()
 controller_1 = PID(Kp=-10, Ki=0.5, Kd=1)
 controller_2 = PID(Kp=-10, Ki=0.5, Kd=1)
 controller_3 = PID(Kp=-10, Ki=0.5, Kd=1)
 
 return_a = 0
 
-for i, current_time in enumerate(env.time):
-    # action, _ = model.predict(obs, deterministic=True)
-    action = np.array([controller_1(env.error[0], current_time),
-                       controller_2(env.error[1], current_time),
-                       controller_2(env.error[2], current_time)
-                       ])
-    obs, reward, done, info = env.step(action)
+for i, current_time in enumerate(env_eval.time):
+    action, _ = model.predict(obs, deterministic=True)
+    # action = np.array([controller_1(env_eval.error[0], current_time),
+    #                    controller_2(env_eval.error[1], current_time),
+    #                    controller_2(env_eval.error[2], current_time)
+    #                    ])
+    obs, reward, done, info = env_eval.step(action)
     return_a += reward
-    if current_time == env.time[-1]:
+    if current_time == env_eval.time[-1]:
 
         fig = make_subplots(rows=6, cols=2,)
 
         fig.append_trace(go.Scatter(
-            x=env.time, y=env.state_history[0, :].T, name=r'$p [^\circ/s]$',
+            x=env_eval.time, y=env_eval.state_history[0, :].T, name=r'$p [^\circ/s]$',
             line=dict(color='#636EFA')), row=1, col=2)
         fig.append_trace(go.Scatter(
-            x=env.time, y=env.ref_signal[0, :], name=r'$p_{ref} [^\circ/s]$',
+            x=env_eval.time, y=env_eval.ref_signal[0, :], name=r'$p_{ref} [^\circ/s]$',
             line=dict(color='#EF553B', dash='dashdot')), row=1, col=2)
         fig.update_yaxes(title_text=r'$p \:[^\circ/s]$', row=1, col=2)
 
         fig.append_trace(go.Scatter(
-            x=env.time, y=env.state_history[1, :].T, name=r'$q [^\circ/s]$',
+            x=env_eval.time, y=env_eval.state_history[1, :].T, name=r'$q [^\circ/s]$',
             line=dict(color='#636EFA')), row=1, col=1)
         fig.append_trace(go.Scatter(
-            x=env.time, y=env.ref_signal[1, :], name=r'$q_{ref} [^\circ/s]$',
+            x=env_eval.time, y=env_eval.ref_signal[1, :], name=r'$q_{ref} [^\circ/s]$',
             line=dict(color='#EF553B', dash='dashdot')), row=1, col=1)
         fig.update_yaxes(title_text=r'$q \:[^\circ/s]$', row=1, col=1)
 
         fig.append_trace(go.Scatter(
-            x=env.time, y=env.state_history[2, :].T, name=r'$r [^\circ/s]$',
+            x=env_eval.time, y=env_eval.state_history[2, :].T, name=r'$r [^\circ/s]$',
             line=dict(color='#636EFA')), row=2, col=2)
         fig.append_trace(go.Scatter(
-            x=env.time, y=env.ref_signal[2, :], name=r'$r_{ref} [^\circ/s]$',
+            x=env_eval.time, y=env_eval.ref_signal[2, :], name=r'$r_{ref} [^\circ/s]$',
             line=dict(color='#EF553B', dash='dashdot')), row=2, col=2)
         fig.update_yaxes(title_text=r'$r \:[^\circ/s]$', row=2, col=2)
 
         fig.append_trace(go.Scatter(
-            x=env.time, y=env.state_history[3, :].T, name=r'$V [m/s]$',
+            x=env_eval.time, y=env_eval.state_history[3, :].T, name=r'$V [m/s]$',
             line=dict(color='#636EFA')), row=4, col=1)
-        fig.update_yaxes(title_text=r'$V \:[m/s]$', row=4, col=1)
+        fig.update_yaxes(title_text=r'$V \:[m/s]$', row=4, col=1, title_standoff=0)
 
         fig.append_trace(go.Scatter(
-            x=env.time, y=env.state_history[4, :].T, name=r'$\alpha [^\circ]$',
+            x=env_eval.time, y=env_eval.state_history[4, :].T, name=r'$\alpha [^\circ]$',
             line=dict(color='#636EFA')), row=2, col=1)
         fig.update_yaxes(title_text=r'$\alpha \:[^\circ]$', row=2, col=1)
         fig.append_trace(go.Scatter(
-            x=env.time, y=env.state_history[5, :].T, name=r'$\beta [^\circ]$',
+            x=env_eval.time, y=env_eval.state_history[5, :].T, name=r'$\beta [^\circ]$',
             line=dict(color='#636EFA')), row=4, col=2)
         fig.update_yaxes(title_text=r'$\beta \:[^\circ]$', row=4, col=2)
         fig.append_trace(go.Scatter(
-            x=env.time, y=env.state_history[6, :].T, name=r'$\phi [^\circ]$',
+            x=env_eval.time, y=env_eval.state_history[6, :].T, name=r'$\phi [^\circ]$',
             line=dict(color='#636EFA')), row=3, col=2)
         fig.update_yaxes(title_text=r'$\phi \:[^\circ]$', row=3, col=2)
         fig.append_trace(go.Scatter(
-            x=env.time, y=env.state_history[7, :].T, name=r'$\theta [^\circ]$',
+            x=env_eval.time, y=env_eval.state_history[7, :].T, name=r'$\theta [^\circ]$',
             line=dict(color='#636EFA')), row=3, col=1)
         fig.update_yaxes(title_text=r'$\theta \:[^\circ]$', row=3, col=1)
 
         fig.append_trace(go.Scatter(
-            x=env.time, y=env.state_history[9, :].T, name=r'$H [m]$',
+            x=env_eval.time, y=env_eval.state_history[9, :].T, name=r'$H [m]$',
             line=dict(color='#636EFA')), row=5, col=1)
         fig.update_yaxes(title_text=r'$H \:[m]$', row=5, col=1)
 
         fig.append_trace(go.Scatter(
-            x=env.time, y=env.action_history[0, :].T,
+            x=env_eval.time, y=env_eval.action_history[0, :].T,
             name=r'$\delta_e [^\circ]$', line=dict(color='#00CC96')), row=6, col=1)
         fig.update_yaxes(title_text=r'$\delta_e \:[^\circ]$', row=6, col=1)
         fig.append_trace(go.Scatter(
-            x=env.time, y=env.action_history[1, :].T,
+            x=env_eval.time, y=env_eval.action_history[1, :].T,
             name=r'$\delta_a [^\circ]$', line=dict(color='#00CC96')), row=5, col=2)
         fig.update_yaxes(title_text=r'$\delta_a \:[^\circ]$', row=5, col=2)
         fig.append_trace(go.Scatter(
-            x=env.time, y=env.action_history[2, :].T,
+            x=env_eval.time, y=env_eval.action_history[2, :].T,
             name=r'$\delta_r [^\circ]$', line=dict(color='#00CC96')), row=6, col=2)
         fig.update_yaxes(title_text=r'$\delta_r \:[^\circ]$', row=6, col=2)
 
@@ -130,7 +131,7 @@ for i, current_time in enumerate(env.time):
             t=10,
         ))
 
-        end_time = env.time[-1] + env.dt * 2
+        end_time = env_eval.time[-1] + env_eval.dt * 2
         fig.update_xaxes(title_text="Time [s]", range=[0, end_time], tickmode='array',
                          tickvals=np.arange(0, end_time, 5), row=6, col=1)
         fig.update_xaxes(title_text="Time [s]", range=[0, end_time], tickmode='array',
