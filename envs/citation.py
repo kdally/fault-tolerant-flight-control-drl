baec6195365a6edc5fc2a10dc7485125d910b3a0
Killian Dally <k.dally@student.tudelft.nl>
2020-09-09 22:22:10 +0200

    simplified C model

--- envs/citation.py
+++ envs/citation.py
@@ -1,22 +1,26 @@
 # Import the low-level C/C++ module
 if __package__ or "." in __name__:
-    from . import _citation as citation
+    from . import _citation as C_MODEL
 else:
-    import _citation as citation
-import numpy as np
+    import _citation as C_MODEL
+
 
 def initialize():
-    return citation.initialize()
-
+    return C_MODEL.initialize()
 
 def terminate():
-    return citation.terminate()
+    return C_MODEL.terminate()
 
+def step(cmd):
+    return C_MODEL.step(cmd)
 
-def step(cmd, env, failure):
-    return citation.step(cmd, env, failure)
 
 initialize()
 
-print(step(np.zeros(10),np.zeros(9),0))
+import numpy as np
+
+initialize()
+print(step(np.ones(10)))
+
+print(step(np.ones(10)))
 
